2eb5bd7313a86be2e7b3f9e21a039f78747a2370
Reese Richardson <59063704+reeserich@users.noreply.github.com>
2024-05-29 11:21:44 -0500

    version 1.3.0 updates

---
+++ scripts/generate_macos_icons.sh
@@ -0,0 +1,7 @@
+convert ../assets/icons/logo.png -resize 16x16 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_16.png
+convert ../assets/icons/logo.png -resize 32x32 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_32.png
+convert ../assets/icons/logo.png -resize 64x64 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_64.png
+convert ../assets/icons/logo.png -resize 128x128 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_128.png
+convert ../assets/icons/logo.png -resize 256x256 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_256.png
+convert ../assets/icons/logo.png -resize 512x512 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_512.png
+convert ../assets/icons/logo.png -resize 1024x1024 ../macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_1024.png
